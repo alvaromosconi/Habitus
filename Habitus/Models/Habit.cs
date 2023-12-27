fdcb668bd94da18d78186a518c56590595e74cb0
Álvaro Mosconi <alvaromosconi@outlook.com>
2023-12-27 17:21:07 -0300

    feat: models for basic app functionality

---
+++ Habitus/Models/Habit.cs
@@ -0,0 +1,41 @@
+﻿namespace Habitus.Models;
+
+public class Habit
+{
+    public int Id { get; set; }
+
+    public string UserID { get; set; }
+
+    public HabitusUser User;
+
+    public Category Category;
+
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; }
+
+    public string? Description { get; set; }
+    
+    public TimeSpan NotificationTime { get; set; }
+
+    public HabitFrequency Frequency { get; set; } = HabitFrequency.Daily;
+
+    public HabitState State { get; set; } = HabitState.Active;
+}
+
+public enum HabitFrequency
+{ 
+    Minutely,
+    Hourly,
+    Daily,
+    Weekly,
+    Monthly,
+    Custom
+}
+
+public enum HabitState
+{
+    Active,
+    Inactive
+}
+
---
+++ Habitus/Models/HabitusUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Habitus.Models;
+
+public class HabitusUser : IdentityUser
+{
+    public ICollection<Habit> UserHabits { get; set; }
+}
